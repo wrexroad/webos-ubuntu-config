29f1e3af31fec6698fa886a3d7cad89643abb1a6
Warren <wrexroad@gmail.com>
2013-07-30 14:18:56 -0700

    Initial version

---
+++ runme.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function newuser {
+   read -p "Enter your username: " user;
+   id $user > /dev/null 2> /dev/null
+   if [ $? -eq 0 ]
+   then
+      echo "User exists!"
+      getuser
+      return 0
+   fi
+   adduser $user
+}
+
+function fixupstart {
+#make upstart work
+   dpkg-divert --local --rename --add /sbin/initctl
+   ln -s /bin/true/ /sbin/initctl
+}
+
+#change apt repo to oneiric
+sed -i "s/natty/oneiric/g" /etc/apt/sources.list
+
+#upgrade the system
+apt-get update -y
+fixupstart
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get install -y update-manager-core
+do-release-upgrade
+
+#install packages
+apt-get install -y lxde-core chromium-browser
+
+#create new user
+#newuser
+
+#clean up
+rm ubuntu-natty-chroot.tgz
